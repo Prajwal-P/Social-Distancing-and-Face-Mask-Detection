2dd40ad67c89d980f1d0aa27725cfbe0fdd44154
Prajwal-P <prajwal72p@gmail.com>
2021-07-06 15:14:51 +0530

    removed unused imports

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
 import numpy as np
-import time
 import cv2
-import math
 from modules.detection import detect_people
 from scipy.spatial import distance as dist
 from modules.config import camera_no
